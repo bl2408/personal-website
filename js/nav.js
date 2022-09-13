3a291b7cc4de9b2a63092252e3d00b87e807f938
Brian Lambert <brianlambert2408@gmail.com>
2022-09-13 17:49:34 +1000

    Added nav styling and features

--- js/nav.js
+++ js/nav.js
@@ -1,27 +1,45 @@
 const blNav = {
     navId:          'nav-main-top',
     hamId:          'nav-main-top-ham',
+    overlayId:      'overlay',
+    overlayOpacity: 0.8,
 };
 
-const BLNavHamButton =(close=false)=>{
-    if(close===true){
+const BLNavToggle =(close=false)=>{
+    if(blNav.navEl.style.maxHeight || close===true){
         blNav.navEl.style.maxHeight = null;
-        return;
-    }
-    if(blNav.navEl.style.maxHeight){
-        blNav.navEl.style.maxHeight = null;
+        blNav.overlayEl.style.visibility = null;
+        blNav.overlayEl.style.opacity = null;
+        blNav.hamEl.classList.add("ham-open");
+        blNav.hamEl.classList.remove("ham-close");
     }else{
         blNav.navEl.style.maxHeight = `${blNav.navEl.scrollHeight}px`;
+        blNav.overlayEl.style.visibility = 'visible';
+        blNav.overlayEl.style.opacity = blNav.overlayOpacity;
+        blNav.hamEl.classList.add("ham-close");
+        blNav.hamEl.classList.remove("ham-open");
     } 
 };
 
 window.addEventListener("load", ()=>{
     blNav.navEl = document.querySelector(`#${blNav.navId}`);
     blNav.hamEl = document.querySelector(`#${blNav.hamId}`);
-    
-    blNav.hamEl.addEventListener("click", BLNavHamButton);
+    blNav.overlayEl = document.querySelector(`#${blNav.overlayId}`);
+
+    blNav.overlayEl.addEventListener("click", ()=>BLNavToggle(true));
+
+    blNav.hamEl.addEventListener("click", BLNavToggle);
 });
 
 window.addEventListener("scroll", ()=>{
-    BLNavHamButton(true);
+    BLNavToggle(true);
 });
+
+window.addEventListener("resize", ()=>{
+    BLNavToggle(true);
+});
+
+const BLNavClick=(el)=>{
+    BLNavToggle(true);
+    BLScroll(el);
+};
